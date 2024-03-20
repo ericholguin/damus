54c829709ad9d5bacbeec54ff892cf009e1e36d6
ericholguin <ericholguin@apache.org>
2024-03-19 19:11:46 -0600

    ux: Local Search Redesign
    This patch redesigns the pull down search view by improving the search bar.
    In addition minor ux/ui improvements are done such as:
    - hiding the timeline when the user is searching
    - hiding the custom picker when the user is searching
    - text suggesting the user try searching for keywords
    - dismiss the keyboard on scroll
    
    Testing
    ——
    iPhone 15 Pro Max (17.3.1) Dark Mode:
    https://v.nostr.build/WGGzk.mp4
    
    iPhone SE (3rd generation) (16.4) Light Mode:
    https://v.nostr.build/Q77Vv.mp4
    
    ——
    
    Fixes: https://github.com/damus-io/damus/issues/2074
    Closes: https://github.com/damus-io/damus/issues/2046
    Changelog-Fixed: search results remaining after empty search
    Changelog-Changed: local search ux/ui
    
    Signed-off-by: ericholguin <ericholguin@apache.org>

--- damus/ContentView.swift
+++ damus/ContentView.swift
@@ -80,6 +80,12 @@ struct ContentView: View {
     @State var confirm_overwrite_mutelist: Bool = false
     @SceneStorage("ContentView.filter_state") var filter_state : FilterState = .posts_and_replies
     @State private var isSideBarOpened = false
+    @State var search: String = ""
+    @State private var results: [NostrEvent] = []
+    @State var initialOffset: CGFloat?
+    @State var offset: CGFloat?
+    @State var showSearch: Bool = true
+    @FocusState private var isSearchFocused: Bool
     var home: HomeModel = HomeModel()
     @StateObject var navigationCoordinator: NavigationCoordinator = NavigationCoordinator()
     @AppStorage("has_seen_suggested_users") private var hasSeenOnboardingSuggestions = false
@@ -117,7 +123,7 @@ struct ContentView: View {
                 }
                 .tabViewStyle(.page(indexDisplayMode: .never))
                 
-                if privkey != nil {
+                if privkey != nil && (!isSearchFocused && search.isEmpty) {
                     PostButtonContainer(is_left_handed: damus_state?.settings.left_handed ?? false) {
                         self.active_sheet = .post(.posting(.none))
                     }
@@ -125,21 +131,59 @@ struct ContentView: View {
             }
         }
         .safeAreaInset(edge: .top, spacing: 0) {
-            VStack(spacing: 0) {
-                CustomPicker(selection: $filter_state, content: {
-                    Text("Notes", comment: "Label for filter for seeing only notes (instead of notes and replies).").tag(FilterState.posts)
-                    Text("Notes & Replies", comment: "Label for filter for seeing notes and replies (instead of only notes).").tag(FilterState.posts_and_replies)
-                })
-                Divider()
-                    .frame(height: 1)
+            if !isSearchFocused && search.isEmpty {
+                VStack(spacing: 0) {
+                    CustomPicker(selection: $filter_state, content: {
+                        Text("Notes", comment: "Label for filter for seeing only notes (instead of notes and replies).").tag(FilterState.posts)
+                        Text("Notes & Replies", comment: "Label for filter for seeing notes and replies (instead of only notes).").tag(FilterState.posts_and_replies)
+                    })
+                    Divider()
+                        .frame(height: 1)
+                }
+                .background(colorScheme == .dark ? Color.black : Color.white)
+                .transition(.opacity)
             }
-            .background(colorScheme == .dark ? Color.black : Color.white)
         }
     }
     
     func contentTimelineView(filter: (@escaping (NostrEvent) -> Bool)) -> some View {
-        TimelineView(events: home.events, loading: .constant(false), damus: damus_state, show_friend_icon: false, filter: filter) {
-            PullDownSearchView(state: damus_state, on_cancel: {})
+        VStack {
+            if self.showSearch {
+                PullDownSearchView(state: damus_state, search_text: $search, results: $results)
+                    .focused($isSearchFocused)
+                    .transition(.move(edge: .top))
+            }
+            if !isSearchFocused && search.isEmpty {
+                TimelineView(events: home.events, loading: .constant(false), damus: damus_state, show_friend_icon: false, filter: filter) {
+                    GeometryReader { geometry in
+                        Color.clear.preference(key: OffsetKey.self, value: geometry.frame(in: .global).minY)
+                            .frame(height: 0)
+                    }
+                }
+            } else {
+                SearchContentView(state: damus_state, search_text: $search, results: $results)
+                    .scrollDismissesKeyboard(.immediately)
+            }
+        }
+        .onPreferenceChange(OffsetKey.self) {
+            if self.initialOffset == nil || self.initialOffset == 0 {
+                self.initialOffset = $0
+            }
+            
+            self.offset = $0
+            
+            guard let initialOffset = self.initialOffset,
+                  let offset = self.offset else {
+                return
+            }
+            
+            if(initialOffset > offset){
+                withAnimation(.easeOut(duration: 0.2)) {
+                    self.showSearch = false
+                }
+            } else {
+                self.showSearch = true
+            }
         }
     }
     
@@ -781,6 +825,14 @@ struct ContentView: View {
         case .profile_zap:
             break
         }
+    }
+}
+
+struct OffsetKey: PreferenceKey {
+    static let defaultValue: CGFloat? = nil
+    static func reduce(value: inout CGFloat?,
+                       nextValue: () -> CGFloat?) {
+        value = value ?? nextValue()
     }
 }
 
